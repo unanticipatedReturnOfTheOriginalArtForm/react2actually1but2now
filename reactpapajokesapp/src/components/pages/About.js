1498c6bd04d18bf971f0685882c4afaee69f01d9
unanticipatedReturnOfTheOriginalArtForm <88819495+unanticipatedReturnOfTheOriginalArtForm@users.noreply.github.com>
2021-08-25 17:51:38 +0100

    Add files via upload

--- reactpapajokesapp/src/components/pages/About.js
+++ reactpapajokesapp/src/components/pages/About.js
@@ -1,1 +1,10 @@
+import React from 'react'
 
+export default function About() {
+    return (
+        <div>
+            <h1>Dave this is a react application.. </h1>
+            <p>Dave this app demos React Hooks, Functional Components PLUS React Router</p>
+        </div>
+    )
+}
